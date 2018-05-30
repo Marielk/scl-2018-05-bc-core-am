42d34c920ce469aade4d6d881ab1a35c59b2748e
Marielk <marielk.mya@gmail.com>
2018-05-30 11:37:17 -0300

    trabajo en las funciones intentando hacerlo cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,47 @@
+
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+ encode: (userText, positions) => {
+    let asciiValue = ""; // aca guardare los valores ascii de cada letra
+    let fullTextCipher = ""; // aca guardare el texto completo cifrado
+    for (let i = 0; i < userText.length; i++) { // este es un ciclo que recorre el texto ingresado
+      
+      asciiValue += userText[i].charCodeAt(i); // esto va sumando cada letra al valor ascii
+      const newPosition = (asciiValue - 65 + positions) % 26 + 65; // esto calcula la nueva posicion assi cifrada
+      alert(newPosition); // esto nos muestra la nueva poscion 
+      const resultChiper= String.fromCharCode(); // esto transforma esa nueva posicion a una letra del alfabeto normal
+      console.log(resultChiper); // esto nos muestra la nueva letra 
+  }
+ }
+}
+
+
+
+
+
+/* 
+
+window.cipher = {
+  encode: (userText, positions) => {
+     
+    } 
+  }
+    /* Acá va tu código 
   },
   decode: () => {
-    /* Acá va tu código */
+    /* Acá va tu código 
   }
+} 
+console.log((str.charCodeAt(i) - 65 + offset) % 26 +65); */
+/* const str = "HOLA ESTE ES UN STRING"; 
+let result = "";
+for = (let i = 0; i <str.length; i++){
+           i= indice o posicion dentro de la palabra           posicion dentro de la palabra contenida en la variable
+  console.log('Esta es la iteracion numero: ' +i+ ', letra: ' +str[i]);
+  result += str[i].toLowerCase();
+  console.log('El nuevo string: '+result);
 }
+
+const asciiValue = userText.charCodeAt(i);
+     console.log(asciiValue);
+     const resultChiper = (asciiValue - 65 + positions) % 26 + 65; */
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const userText = document.getElementById("textToCipher"). value;
+
+const btn = document.getElementById("cipherButton");
+
+const positions = document.getElementById("placeNumber"). value;
+
+btn.addEventListener('click', cipher.encode);
+
+const cipherResult = document.getElementById("cipherText"). value; 
+document.getElementById("cipherText").innerHTML = cipherResult; 
+
 /* Acá va tu código */
